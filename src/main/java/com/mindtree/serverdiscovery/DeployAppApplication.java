ebf09c407f7d01153e9f15c5b9d8bb98f8ddbcaf
Somesh_Banna <someshbanna845@gmail.com>
2023-02-23 15:30:56 +0530

    deploy statement added

--- src/main/java/com/mindtree/serverdiscovery/DeployAppApplication.java
+++ src/main/java/com/mindtree/serverdiscovery/DeployAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeployAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Deploy App");
 		SpringApplication.run(DeployAppApplication.class, args);
 	}
 
